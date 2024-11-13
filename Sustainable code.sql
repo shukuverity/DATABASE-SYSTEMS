455073e028da267a82d299cd9b3eaf3e7b0d9ad1
Steve leone <steve.muturi@strathmore.edu>
2024-11-13 10:48:31 +0300

    Update Sustainable code.sql

--- Sustainable code.sql
+++ Sustainable code.sql
@@ -1,1 +1,14 @@
+-- Create CropTypes Table
+CREATE TABLE CropTypes (
+	CropTypeID INT PRIMARY KEY,
+	CropTypeName VARCHAR(100) NOT NULL
+);
+
+-- Create Farmers Table
+CREATE TABLE Farmers (
+	FarmerID INT PRIMARY KEY,
+	Name VARCHAR(100) NOT NULL,
+	Location VARCHAR(255),
+	ContactInfo VARCHAR(100)
+);
 
